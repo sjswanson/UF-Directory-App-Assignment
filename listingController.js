16ac1b05d4eb7a2eb251d270aba118d9bacfdcac
SAM <samueljswanson@ufl.edu>
2017-09-05 17:45:19 -0400

    Finished Assignment 2; new listings at end

--- listingController.js
+++ listingController.js
@@ -7,10 +7,26 @@ angular.module('listings').controller('ListingsController', ['$scope', 'Listings
       Implement these functions in the controller to make your application function
       as described in the assignment spec.
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {
-      $scope.detailedInfo = Listings[index];
+    $scope.addListing = function() {
+
+      $scope.listings.push({
+        index: $scope.listings.length,
+        name: $scope.newName,
+        code: $scope.newCode,
+        address: $scope.newAddress,
+        coordinates: {
+          latitude: $scope.newLatitude,
+          longitude: $scope.newLongitude
+        }
+      });
+    };
+    $scope.deleteListing = function(item) {
+      var delIndex = $scope.listings.indexOf(item);
+      $scope.listings.splice(delIndex,1);
+    };
+    $scope.showDetails = function(item) {
+      var showIndex = $scope.listings.indexOf(item);
+      $scope.detailedInfo = Listings[showIndex];
     };
   }
 ]);
