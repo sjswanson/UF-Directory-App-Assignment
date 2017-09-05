2406f8da3f7109c26ce5986c24176f015ccc68dd
Samuel J. Swanson <swanson@lin309-07.cise.ufl.edu>
2017-09-05 15:51:33 -0400

    sam's first commit

--- listingController.js
+++ listingController.js
@@ -1,14 +1,16 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
+    /*
+      Implement these functions in the controller to make your application function
+      as described in the assignment spec.
      */
     $scope.addListing = function() {};
     $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.showDetails = function(index) {
+      $scope.detailedInfo = Listings[index];
+    };
   }
 ]);
